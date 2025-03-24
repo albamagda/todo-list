e3d2327252b48aa52e9898e20f56199881b044b8
INDRA\amagdaleno <amagdaleno@indra.es>
2025-03-24 10:32:14 +0100

    Tarea service en proceso

---
+++ src/main/java/todoList/To_do_List/Service/TareaService.java
@@ -0,0 +1,29 @@
+package todoList.To_do_List;
+
+import java.util.HashMap;
+import java.util.Map;
+import todoList.To_do_List.model.Tarea;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TareaService {
+    private Map<String, Tarea> todoList = new HashMap<>();
+
+    public Map<String, Tarea> obtenerTarea(){
+        return todoList;
+    }
+
+    public void aniadirTarea (Tarea tarea){
+        todoList.put(tarea.getNombre(), tarea);
+    }
+
+    public boolean eliminarTarea (String nombre){
+        Tarea tarea = todoList.get(nombre);
+        boolean flag = false;
+        if(tarea != null ){
+            agenda.remove(nombre);
+            flag = true;
+        }
+        return flag;
+    }
+}
